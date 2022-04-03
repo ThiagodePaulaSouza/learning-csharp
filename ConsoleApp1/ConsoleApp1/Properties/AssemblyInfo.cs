c3b1f6625838ae7fbb6e91b39cd07d04b1eede53
ThiagodePaulaSouza <thiagodepaulasouza@gmail.com>
2022-04-03 13:09:58 -0300

    .

--- ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
+++ ConsoleApp1/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // As informações gerais sobre um assembly são controladas por
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associada a um assembly.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Teste")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Teste")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
