c3134d094e699f46164c3eb504039f4f0b1215c2
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-04-08 14:15:16 -0300

    Converter Dados

--- ConsoleApp1/ConsoleApp1/TesteConsole.cs
+++ ConsoleApp1/ConsoleApp1/TesteConsole.cs
@@ -10,39 +10,59 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            #region entrada
-            string nome1, nome2, nome3, nome4, aux;
+            #region Conversão Implícita
+            //byte num1 = 12;
+            //ushort num2;
 
-            Console.WriteLine("digite o nome #1: ");
-            nome1 = Console.ReadLine();
-            Console.WriteLine("digite o nome #2: ");
-            nome2 = Console.ReadLine();
-            Console.WriteLine("digite o nome #3: ");
-            nome3 = Console.ReadLine();
-            Console.WriteLine("digite o nome #4: ");
-            nome4 = Console.ReadLine();
+            //float num3 = 123.1f;
+            //double num4;
+
+            //char letra = 'C';
+            //int numero = 'C';
+
+            //Console.WriteLine($"inteiros: {num2 = num1}\nDecimais {num4 = num3}\nCaracter {letra} em int é {numero}");
+            //Console.ReadKey();
             #endregion
 
-            #region inverter
-            aux = nome1;
-            nome1 = nome4;
-            nome4 = aux;
+            #region Conversão Explícita
+            //ushort num1 = 100;
+            //byte num2 = (byte)num1;
 
-            aux = nome2;
-            nome2 = nome3;
-            nome3 = aux;
+            //float num3 = 078123.0189272f;
+            //int num4 = (int)num3;
+
+            //char letra = (char)12;
+
+            //Console.WriteLine(num2);
+            //Console.WriteLine(num3);
+            //Console.WriteLine(letra);
+            //Console.ReadKey();
             #endregion
 
-            #region mostrar
-            Console.WriteLine();
-            Console.WriteLine("nome invertivo na sequência invertida");
-            Console.WriteLine(nome1);
-            Console.WriteLine(nome2);
-            Console.WriteLine(nome3);
-            Console.WriteLine(nome4);
-            Console.ReadKey();
+            #region Método Parse
+            //string textoNumero = "1985";
+            //int numero = int.Parse(textoNumero); //erro System.FormatException: 'A cadeia de caracteres de entrada não estava em um formato correto.'
+
+            //byte num1 = byte.Parse("123");
+            //double num2 = double.Parse("62,213");//depende da linguagem do sistema (en = .) || (br = ,)
+
+
+            //Console.WriteLine(numero);
+            //Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+            //Console.ReadKey();
             #endregion
 
+            #region Metodo Converse
+
+            //string texto = Convert.ToString(5486795306798);
+            //double num1 = Convert.ToDouble(true);
+
+            //Console.WriteLine(texto);
+            //Console.WriteLine(num1);
+            //Console.ReadLine();
+
+            #endregion
         }
     }
 }
