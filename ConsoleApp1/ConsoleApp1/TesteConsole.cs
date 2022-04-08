2b16f27baafb7078849849cb540bb2ed2db93976
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-04-08 14:45:02 -0300

    converter temp

--- ConsoleApp1/ConsoleApp1/TesteConsole.cs
+++ ConsoleApp1/ConsoleApp1/TesteConsole.cs
@@ -10,59 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            #region Conversão Implícita
-            //byte num1 = 12;
-            //ushort num2;
+            //conversor de temperatura
+            double c, f, k;
 
-            //float num3 = 123.1f;
-            //double num4;
-
-            //char letra = 'C';
-            //int numero = 'C';
-
-            //Console.WriteLine($"inteiros: {num2 = num1}\nDecimais {num4 = num3}\nCaracter {letra} em int é {numero}");
-            //Console.ReadKey();
-            #endregion
-
-            #region Conversão Explícita
-            //ushort num1 = 100;
-            //byte num2 = (byte)num1;
-
-            //float num3 = 078123.0189272f;
-            //int num4 = (int)num3;
-
-            //char letra = (char)12;
-
-            //Console.WriteLine(num2);
-            //Console.WriteLine(num3);
-            //Console.WriteLine(letra);
-            //Console.ReadKey();
-            #endregion
-
-            #region Método Parse
-            //string textoNumero = "1985";
-            //int numero = int.Parse(textoNumero); //erro System.FormatException: 'A cadeia de caracteres de entrada não estava em um formato correto.'
-
-            //byte num1 = byte.Parse("123");
-            //double num2 = double.Parse("62,213");//depende da linguagem do sistema (en = .) || (br = ,)
-
-
-            //Console.WriteLine(numero);
-            //Console.WriteLine(num1);
-            //Console.WriteLine(num2);
-            //Console.ReadKey();
-            #endregion
-
-            #region Metodo Converse
-
-            //string texto = Convert.ToString(5486795306798);
-            //double num1 = Convert.ToDouble(true);
-
-            //Console.WriteLine(texto);
-            //Console.WriteLine(num1);
-            //Console.ReadLine();
-
-            #endregion
+            Console.WriteLine("Converter Temperatura");
+            Console.WriteLine("insia a temperatura em Celsius: ");
+            c = double.Parse(Console.ReadLine());
+            f = (c * 9 / 5) + 322;
+            k = c + 273.15;
+            Console.WriteLine($"o celsius é {c}\nO fahrenheit é {f}\nO kelvin é {k} ");
+            Console.ReadKey();
         }
     }
 }
