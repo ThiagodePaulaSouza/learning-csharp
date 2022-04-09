4f5e0331f6cf9d94b742788bd4feb02bfa793f8f
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-04-09 20:38:58 -0300

    IMC Simples

--- ConsoleApp1/ConsoleApp1/TesteConsole.cs
+++ ConsoleApp1/ConsoleApp1/TesteConsole.cs
@@ -10,37 +10,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Multiplicação de Matriz
-            int[,] m1 = new int[2, 3];
-            int[,] m2 = new int[3, 2];
-            int[,] resultado = new int[2, 2];
+            //imc peso / (altura²)
+            Console.WriteLine("calcular IMC");
 
-            Console.WriteLine("PREENCHA A MATRIZ 1");
-            for (int linha = 0; linha < 2; linha++)
+            Console.WriteLine("Digite o peso: ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double IMC = peso / (altura * altura);
+            if (IMC < 20)
             {
-                for (int coluna = 0; coluna < 3; coluna++)
-                {
-                    Console.Write($"#1. posição [{linha}][{coluna}] = ");
-                    m1[linha, coluna] = int.Parse(Console.ReadLine());
-                }
+                Console.WriteLine($"\n{IMC} você está MUITO MAGRO");
+            }
+            else if (IMC >= 20 && IMC <=24)
+            {
+                Console.WriteLine($"\n{IMC} você está NORMAL");
+            }
+            else if (IMC >= 25 && IMC <= 29)
+            {
+                Console.WriteLine($"\n{IMC} você está GORDO");
+            }
+            else if (IMC >= 30 && IMC <= 34)
+            {
+                Console.WriteLine($"\n{IMC} você está MUITO GORDO");
+            }
+            else
+            {
+                Console.WriteLine($"\n{IMC} você está EXTREMAMENTE GORDO");
             }
 
-            Console.WriteLine("\nPREENCHA A MATRIZ 2");
-            for (int linha = 0; linha < 3; linha++)
-            {
-                for (int coluna = 0; coluna < 2; coluna++)
-                {
-                    Console.Write($"#2. posição [{linha}][{coluna}] = ");
-                    m2[linha, coluna] = int.Parse(Console.ReadLine());
-                }
-            }
-            Console.WriteLine("\nResultado da Matriz #1 x Matriz #2");
-            for (int i = 0; i < 2; i++)
-                for (int j = 0; j < 2; j++)
-                    for (int k = 0; k < 3; k++)
-                        resultado[i, j] += m1[i, j] * m2[k, j];
-            Console.WriteLine($"[{resultado[0, 0]}][{resultado[0, 1]}]");
-            Console.WriteLine($"[{resultado[1, 0]}][{resultado[1, 1]}]");
+
             Console.ReadKey();
         }
     }
