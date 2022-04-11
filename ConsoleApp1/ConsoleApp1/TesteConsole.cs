7c363ba2bbb323df2548b2154b2031567f6b129a
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-04-11 01:06:11 -0300

    .

--- ConsoleApp1/ConsoleApp1/TesteConsole.cs
+++ ConsoleApp1/ConsoleApp1/TesteConsole.cs
@@ -10,52 +10,57 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-        //calculadora
-        Inicio:
-            Console.Clear();
-            Console.Write("Digite o primeiro numero: ");
-            double num1 = double.Parse(Console.ReadLine());
-            Console.Write("Digite o segundo numero: ");
-            double num2 = double.Parse(Console.ReadLine());
+            #region calculadora
+            //Inicio:
+            //    Console.Clear();
+            //    Console.Write("Digite o primeiro numero: ");
+            //    double num1 = double.Parse(Console.ReadLine());
+            //    Console.Write("Digite o segundo numero: ");
+            //    double num2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Selecione a operação (+ - x /): ");
-            char op = char.Parse(Console.ReadLine());
-            double resultado=0;
-            switch (op)
-            {
-                default:
-                    Console.WriteLine("Error, Escolha a opção válida");
-                    break;
-                case '+':
-                    resultado = num1 + num2;
-                    break;
-                case '-':
-                    resultado = num1 - num2;
-                    break;
-                case 'x':
-                    resultado = num1 * num2;
-                    break;
-                case '/':
-                    if (num2 == 0)
-                    {
-                        Console.WriteLine("Não é possivel dividir por 0");
-                        Console.ReadKey();
-                        goto Inicio;
-                    }
-                        
-                    else
-                        resultado = num1 / num2;
-                    
-                    
-                    break;
-            }
-            Console.WriteLine($"O resultado é {resultado}");
-            Console.WriteLine("Continuar Calculando (s / n)?");
-            string opcao = Console.ReadLine();
-            if (opcao == "s" || opcao == "S")
-            {
-                goto Inicio;
-            }
+            //    Console.WriteLine("Selecione a operação (+ - x /): ");
+            //    char op = char.Parse(Console.ReadLine());
+            //    double resultado = 0;
+            //    switch (op)
+            //    {
+            //        default:
+            //            Console.WriteLine("Error, Escolha a opção válida");
+            //            break;
+            //        case '+':
+            //        case 'l':
+            //            resultado = num1 + num2;
+            //            break;
+            //        case '-':
+            //            resultado = num1 - num2;
+            //            break;
+            //        case 'x':
+            //            resultado = num1 * num2;
+            //            break;
+            //        case '/':
+            //            if (num2 == 0)
+            //            {
+            //                Console.WriteLine("Não é possivel dividir por 0");
+            //                Console.ReadKey();
+            //                goto Inicio;
+            //            }
+
+            //            else
+            //                resultado = num1 / num2;
+
+
+            //            break;
+            //    }
+            //    Console.WriteLine($"O resultado é {resultado}");
+            //    Console.WriteLine("Continuar Calculando (s / n)?");
+            //    string opcao = Console.ReadLine();
+            //    if (opcao == "s" || opcao == "S")
+            //    {
+            //        goto Inicio;
+            //    }
+            #endregion
+            Console.WriteLine(Solution("teste"));
+            Console.ReadLine();
         }
+        public static string Solution(string str) => new string (str.Reverse().ToArray());
     }
 }
