17bda2173d74c235b1a2d9b281094a9f616a6213
Thiago de Paula Souza <thiagodepaulasouza@gmail.com>
2022-04-11 16:02:34 -0300

    + sealed

---
+++ ConsoleApp1/ConsoleApp1/Humano.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Humano
+    {
+        //pode ser sobrescrito, pode ter implementação || tipo abstrato mas pode reescrever
+        public virtual void Olhos()
+        {
+            Console.WriteLine("Humano tem olho");
+        }
+        public virtual void Cabelos()
+        {
+            Console.WriteLine("Humano tem cabelo");
+        }
+    }
+    class Pessoa : Humano
+    {
+        public override void Cabelos()
+        {
+            Console.WriteLine("Pessoa tem cabelos");
+        }
+
+        public sealed override void Olhos()
+        {
+            Console.WriteLine("Pessoa tem olhos");
+        }
+    }
+    class Homem : Pessoa
+    {
+        public override void Cabelos()
+        {
+            Console.WriteLine("Homem tem cabelos");
+        }
+
+        //public override void Olhos()
+        //{
+        //    Console.WriteLine("Homem tem olhos");
+        //}
+    }
+}
+;
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -87,10 +87,19 @@ namespace ConsoleApp1
 
             #endregion
 
-            Carr c = new Carr();
-            Bicicleta b = new Bicicleta();
-            c.Acelerar();
-            b.Acelerar();
+            Humano a = new Humano();
+            Pessoa b = new Pessoa();
+            Homem c = new Homem();
+
+            Console.WriteLine("\nHumano");
+            a.Olhos();
+            a.Cabelos();
+            Console.WriteLine("\nPessoa");
+            b.Olhos();
+            b.Cabelos();
+            Console.WriteLine("\nHomem");
+            c.Olhos();
+            c.Cabelos();
             Console.ReadKey();
         }
     }
