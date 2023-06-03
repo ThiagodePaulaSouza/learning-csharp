deb8d72f8f8bc81219b3efeb5cc5f6fbfc5208ca
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-06-03 20:57:22 -0300

    learning types

---
+++ w3schools/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace W3schools
+{
+  class Program 
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Bom dia!");
+      Console.WriteLine(3 + 3);
+
+      int a;
+      double b;
+      char c;
+      string d;
+      bool e;
+
+      string nome = "Thiago";
+      Console.WriteLine(nome);
+    
+      int meuNumero;
+      meuNumero = 20;
+      Console.WriteLine(meuNumero);
+
+      double meuDouble = 5.22D;
+      bool meuBool = true;
+      Console.WriteLine(meuDouble.ToString(), meuBool.ToString());
+
+      long myL = 15000000000L;
+      Console.WriteLine(myL);
+
+      float myF = 255.3F;
+      Console.WriteLine(myF);
+
+      double myDou = 124535.3232D;
+      Console.WriteLine(myDou);
+    }
+  }
+}
---
+++ w3schools/README.md
@@ -0,0 +1,18 @@
+C# is used for:
+- Mobile app
+- Desktop app
+- Web app
+- Web services
+- Web sites
+- Games
+- VR
+- DB app
+
+int -- 4bytes -- Stores whole numbers from -2,147,483,648 to 2,147,483,647
+long -- 8bytes -- Sores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+float -- 4bytes -- Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+double -- 8bytes -- Stores fractional numbers. Sufficient for storing 15 decimal digits
+bool -- 1bit -- Stores true or false values
+char -- 2bytes -- Stores a single character/letter, surrounded by single quotes
+string -- 2bytes per character -- Stores a sequence of characters, surrounded by double quotes
+
