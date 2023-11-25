e586824a497e912a3e3bed39d52c52077667ed05
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-11-25 17:29:05 -0300

    feat: change map to controller

---
+++ CloudCustomers/CloudCustomers.API/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudCustomers.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly ILogger<UsersController> logger;
+        public UsersController(ILogger<UsersController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet(Name = "GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok();
+        }
+    }
+}
