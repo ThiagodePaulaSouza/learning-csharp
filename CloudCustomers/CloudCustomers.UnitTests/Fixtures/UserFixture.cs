eb4c07872af211a768e8b308fe45eed19a82d2a5
ThiagoDePaulaSouza <thiagodepaulasouza@gmail.com>
2023-11-25 20:15:48 -0300

    feat: added userFixture

---
+++ CloudCustomers/CloudCustomers.UnitTests/Fixtures/UserFixture.cs
@@ -0,0 +1,16 @@
+﻿using CloudCustomers.API.Models;
+
+namespace CloudCustomers.UnitTests.Fixtures
+{
+    public static class UserFixture
+    {
+        public static List<User> GetTestUsers() => new()
+        {
+            new User(1, "João", "joao@gmail.com", new Address("", "", "")),
+            new User(2, "Mario", "Mario@gmail.com", new Address("", "", "")),
+            new User(3, "José", "José@gmail.com", new Address("", "", "")),
+            new User(4, "Mariana", "Mariana@gmail.com", new Address("", "", "")),
+            new User(5, "Gabriele", "Gabriele@gmail.com", new Address("", "", ""))
+        };
+    }
+}
